4ff3ccfff2d7d5bad6dd7d3967e64bfec376fc02
brandonb <brandontbyfield@gmail.com>
2025-04-28 10:10:46 -0400

    surface setup

--- Concentration/css/main.js
+++ Concentration/css/main.js
@@ -1,13 +1,28 @@
 /*----- constants -----*/
+// Each of the "card" objects will be copied twice,
+// then shuffled and used for the board's cards
+const SOURCE_CARDS = [
+  {img: 'https://i.imgur.com/ZXPKaiN.jpg', matched: false},
+  {img: 'https://i.imgur.com/XMEsZBX.jpg', matched: false},
+  {img: 'https://i.imgur.com/6jX1bMT.jpg', matched: false},
+  {img: 'https://i.imgur.com/yKdqsBv.jpg', matched: false},
+  {img: 'https://i.imgur.com/1BV3HLr.jpg', matched: false},
+  {img: 'https://i.imgur.com/QYmN6Hp.jpg', matched: false},
+  {img: 'https://i.imgur.com/D5pWE05.jpg', matched: false},
+  {img: 'https://i.imgur.com/Ss4Xo3x.jpg', matched: false}
+];
+const CARD_BACK = 'https://i.imgur.com/WoEmI2M.jpg'; 
+
+/*----- app's state (variables) -----*/
 
 
-  /*----- state variables -----*/
+/*----- cached element references -----*/
 
 
-  /*----- cached elements  -----*/
+/*----- event listeners -----*/
 
 
-  /*----- event listeners -----*/
+/*----- functions -----*/
 
 
-  /*----- functions -----*/
+// Initialize all state, then call render()
